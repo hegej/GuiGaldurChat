ffa840626c62db99c10247253452cce4eb107653
hegej <hege.jacobsen12@gmail.com>
2024-03-24 14:09:43 +0100

    Update sessionID

--- chat.js
+++ chat.js
@@ -1,3 +1,23 @@
+if (!localStorage.getItem('sessionId')) {
+    localStorage.setItem('sessionId', generateUUID());
+}
+
+function generateUUID() {
+    var d = new Date().getTime();
+    var d2 = (typeof performance !== 'undefined' && performance.now && (performance.now()*1000)) || 0;
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16;
+        if(d > 0){
+            r = (d + r) % 16 | 0;
+            d = Math.floor(d/16);
+        } else {
+            r = (d2 + r) % 16 | 0;
+            d2 = Math.floor(d2/16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     let username = '';
     const chatWindow = document.getElementById('chatWindow');
@@ -21,26 +41,27 @@ document.addEventListener("DOMContentLoaded", function() {
 
     promptUsername();
 
+    let isFirstMessage = true;
+
     async function sendMessage() {
         const inputField = document.getElementById('userInput');
         let userInput = inputField.value;
         inputField.value = '';
 
-        const isFirstMessage = chatWindow.children.length === 0;
-        
         let messageToSend = {
-            username: username,
-            userInput: userInput
+            Username: username,
+            UserInput: userInput, 
+            SessionId: localStorage.getItem('sessionId')
         };
-
+        
         if(isFirstMessage) {
-            delete messageToSend.userInput;
-            messageToSend.userInput = `Navnet mitt er ${username}. ${userInput}`;
+            messageToSend.UserInput = `Navnet mitt er ${username}. ${userInput}`;
+            isFirstMessage = false;
         }
+    
+        chatWindow.innerHTML += `<div class="user-message">${username}: ${userInput}</div>`;    
 
-        chatWindow.innerHTML += `<div class="user-message">${username}: ${userInput}</div>`;
-
-        const response = await fetch('https://vnb78t34-7071.euw.devtunnels.ms/api/Bot/chat', {
+        const response = await fetch('https://hd0mqgsb-7071.euw.devtunnels.ms/api/Bot/chat', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(messageToSend)
@@ -54,10 +75,6 @@ document.addEventListener("DOMContentLoaded", function() {
         const data = await response.json();
         chatWindow.innerHTML += `<div style="text-align: left;">Galdur: ${data.message}</div>`;
         chatWindow.scrollTop = chatWindow.scrollHeight;
-
-        if(isFirstMessage) {
-            isFirstMessage = false;
-        }
     }
 
     window.sendMessage = sendMessage;
